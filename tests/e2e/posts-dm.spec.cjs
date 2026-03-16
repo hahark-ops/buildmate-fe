d9a50a09280db8cd85bbdb648791905733365643
황준수 <hahark@naver.com>
2026-03-16 13:59:49 +0900

    test: wait for comment list reload in e2e

--- tests/e2e/posts-dm.spec.cjs
+++ tests/e2e/posts-dm.spec.cjs
@@ -43,8 +43,14 @@ test('게시글, 댓글, 좋아요, DM, unread 흐름', async ({ browser }) => {
   await pageB.locator('#commentSubmitBtn').click();
   const commentResponse = await commentResponsePromise;
   expect(commentResponse.ok()).toBeTruthy();
+  const commentsFetchPromise = pageB.waitForResponse((response) =>
+    response.url().includes('/comments') && response.request().method() === 'GET' && response.ok()
+  );
   await pageB.reload();
-  await expect(pageB.locator('.comment-content')).toContainText(commentContent);
+  const commentsFetchResponse = await commentsFetchPromise;
+  expect(commentsFetchResponse.ok()).toBeTruthy();
+  await expect(pageB.locator('.comment-item')).toHaveCount(1);
+  await expect(pageB.locator('.comment-content').first()).toHaveText(commentContent);
 
   await pageB.locator('.author-info').click();
   await pageB.getByRole('button', { name: '채팅하기' }).click();
